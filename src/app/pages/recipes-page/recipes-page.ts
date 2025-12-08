daaddd683c84fcd134899e689d47234ddad157bb
UnaxGainzarain <unaxarbizu1@gmail.com>
2025-12-08 21:02:16 +0100

    Conectar la Página recipesPage al servicio

--- src/app/pages/recipes-page/recipes-page.ts
+++ src/app/pages/recipes-page/recipes-page.ts
@@ -1,19 +1,41 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, OnDestroy, signal, inject } from '@angular/core';
+import { Subscription } from 'rxjs'; // Necesario para gestionar la memoria
 import { Recipe } from '../../models/recipe.model';
-// 1. IMPORTANTE: Importar el componente hijo
 import { RecipeList } from '../../components/organisms/recipe-list/recipe-list';
+import { RecipeService } from '../../services/recipe.service';
 
 @Component({
   selector: 'app-recipes-page',
   standalone: true,
-  imports: [RecipeList], // 2. Añadirlo a los imports
+  imports: [RecipeList],
   templateUrl: './recipes-page.html',
-  styleUrl: './recipes-page.scss' // O styles: [] si no tienes scss
+  styleUrl: './recipes-page.scss'
 })
-export class RecipesPage {
-  // 3. Los datos Dummy viven ahora aquí (el "Cerebro")
-  recipes = signal<Recipe[]>([
-    new Recipe('Tortilla', 'Española tradicional', ['Huevos', 'Patatas'], 'https://upload.wikimedia.org/wikipedia/commons/thumb/1/14/Tortilla_de_Patatas.jpg/800px-Tortilla_de_Patatas.jpg'),
-    new Recipe('Pasta', 'Con tomate', ['Pasta', 'Tomate'], 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/23/Pasta_con_pomodoro_fresco_e_basilico.jpg/800px-Pasta_con_pomodoro_fresco_e_basilico.jpg')
-  ]);
+export class RecipesPage implements OnInit, OnDestroy {
+  // Inyección de dependencias moderna (Angular 14+)
+  private recipeService = inject(RecipeService);
+  
+  // Variable para guardar la suscripción y poder cancelarla
+  private sub?: Subscription;
+
+  // Signal que alimentará a la vista
+  recipes = signal<Recipe[]>([]);
+
+  ngOnInit(): void {
+    // PATRÓN OBSERVER: Nos suscribimos a las actualizaciones
+    this.sub = this.recipeService.update$.subscribe(() => {
+      // Cuando alguien grite "¡Cambio!", recargamos la lista
+      this.loadRecipes();
+    });
+  }
+
+  loadRecipes() {
+    this.recipeService.getRecipes().subscribe(data => {
+      this.recipes.set(data); // Actualizamos el Signal
+    });
+  }
+
+  ngOnDestroy(): void {
+    this.sub?.unsubscribe();
+  }
 }
